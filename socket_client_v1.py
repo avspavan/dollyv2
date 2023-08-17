f9d8a37a69ec5d59377e63c89c3e2165b4d15e82
pavan <pavan.akkisetty@intel.com>
2023-08-16 22:41:24 -0700

    increased buffer size

--- socket_client_v1.py
+++ socket_client_v1.py
@@ -1,8 +1,8 @@
 import socket
 
 def get_generated_text(prompt):
-    host = "localhost"
-    port = 8080
+    host = "35.163.197.34"
+    port = 8000
 
     # Create a socket object
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,12 +11,12 @@ def get_generated_text(prompt):
     sock.connect((host, port))
 
     # Send the prompt to the server
-    sock.sendall(prompt.encode())
+    sock.sendall(prompt.encode(), 2048)
 
     # Receive the generated text from the server
     generated_text = ""
     while True:
-        data = sock.recv(1024).decode()
+        data = sock.recv(2048).decode()
         print(data, end="", flush=True)
         if data == "STREAMING_COMPLETE":
             break
@@ -32,11 +32,11 @@ if __name__ == "__main__":
     try:
         with open(file_path, 'r') as file:
             file_content = file.read()
-            print("File content:\n", file_content)  # Print the content of the file
+            #print("File content:\n", file_content)  # Print the content of the file
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
     except Exception as e:
         print("An error occurred:", str(e))
 
-    prompt = "Summarize the paragraph" + file_content
+    prompt = "Summarize the paragraph and use only the content provided" + file_content
     generated_text = get_generated_text(prompt)
